5e4ab6ac9de6dc3f4fe21b0e2d6aeb7848ff7eb0
lovell112 <xuansontrannguyen06@gmail.com>
2025-10-21 00:06:53 +0700

    delete login & sign in

--- Fight.Designer.cs
+++ Fight.Designer.cs
@@ -41,44 +41,49 @@
             this.progressBar2 = new System.Windows.Forms.ProgressBar();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.groupBox1.BackColor = System.Drawing.Color.DarkCyan;
+            this.groupBox1.Controls.Add(this.panel1);
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.progressBar1);
-            this.groupBox1.Controls.Add(this.chieu2);
-            this.groupBox1.Controls.Add(this.chieu1);
-            this.groupBox1.Location = new System.Drawing.Point(0, 412);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 508);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(984, 200);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1312, 244);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.groupBox1_Paint);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 49);
+            this.textBox2.Location = new System.Drawing.Point(16, 60);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(367, 20);
+            this.textBox2.Size = new System.Drawing.Size(488, 22);
             this.textBox2.TabIndex = 5;
             this.textBox2.Text = "Bulbasaur";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 121);
+            this.progressBar1.Location = new System.Drawing.Point(16, 149);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(367, 30);
+            this.progressBar1.Size = new System.Drawing.Size(489, 37);
             this.progressBar1.TabIndex = 2;
             this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
             // 
@@ -86,9 +91,11 @@
             // 
             this.chieu2.BackColor = System.Drawing.Color.White;
             this.chieu2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.chieu2.Location = new System.Drawing.Point(609, 108);
+            this.chieu2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.chieu2.Location = new System.Drawing.Point(0, 120);
+            this.chieu2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chieu2.Name = "chieu2";
-            this.chieu2.Size = new System.Drawing.Size(337, 79);
+            this.chieu2.Size = new System.Drawing.Size(545, 101);
             this.chieu2.TabIndex = 1;
             this.chieu2.Text = "Tấn công nhanh";
             this.chieu2.UseVisualStyleBackColor = false;
@@ -99,9 +106,11 @@
             this.chieu1.BackColor = System.Drawing.Color.White;
             this.chieu1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.chieu1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.chieu1.Location = new System.Drawing.Point(609, 19);
+            this.chieu1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chieu1.Location = new System.Drawing.Point(0, 0);
+            this.chieu1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chieu1.Name = "chieu1";
-            this.chieu1.Size = new System.Drawing.Size(337, 79);
+            this.chieu1.Size = new System.Drawing.Size(545, 120);
             this.chieu1.TabIndex = 0;
             this.chieu1.Text = "Bão Lá";
             this.chieu1.UseVisualStyleBackColor = false;
@@ -111,9 +120,10 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = global::PokemonProject.Properties.Resources.Bulbasaur;
-            this.pictureBox2.Location = new System.Drawing.Point(0, 199);
+            this.pictureBox2.Location = new System.Drawing.Point(0, 245);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(379, 256);
+            this.pictureBox2.Size = new System.Drawing.Size(505, 315);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
@@ -122,9 +132,10 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::PokemonProject.Properties.Resources.charmander;
-            this.pictureBox1.Location = new System.Drawing.Point(637, 84);
+            this.pictureBox1.Location = new System.Drawing.Point(849, 103);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(336, 194);
+            this.pictureBox1.Size = new System.Drawing.Size(448, 239);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -135,36 +146,41 @@
             this.groupBox2.BackColor = System.Drawing.Color.DarkCyan;
             this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Controls.Add(this.progressBar2);
-            this.groupBox2.Location = new System.Drawing.Point(632, 0);
+            this.groupBox2.Location = new System.Drawing.Point(843, 0);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(352, 91);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(469, 112);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(5, 16);
+            this.textBox1.Location = new System.Drawing.Point(7, 20);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.Size = new System.Drawing.Size(159, 22);
             this.textBox1.TabIndex = 4;
             this.textBox1.Text = "Charmander";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // progressBar2
             // 
-            this.progressBar2.Location = new System.Drawing.Point(138, 16);
+            this.progressBar2.Location = new System.Drawing.Point(184, 20);
+            this.progressBar2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.progressBar2.Name = "progressBar2";
-            this.progressBar2.Size = new System.Drawing.Size(208, 20);
+            this.progressBar2.Size = new System.Drawing.Size(277, 25);
             this.progressBar2.TabIndex = 3;
             // 
             // pictureBox3
             // 
             this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox3.Image = global::PokemonProject.Properties.Resources.GrassAtk;
-            this.pictureBox3.Location = new System.Drawing.Point(286, 239);
+            this.pictureBox3.Location = new System.Drawing.Point(425, 309);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(147, 145);
+            this.pictureBox3.Size = new System.Drawing.Size(196, 178);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 3;
             this.pictureBox3.TabStop = false;
@@ -173,30 +189,43 @@
             // 
             this.axWindowsMediaPlayer1.Enabled = true;
             this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
             this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
             this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(210, 26);
             this.axWindowsMediaPlayer1.TabIndex = 4;
             this.axWindowsMediaPlayer1.Visible = false;
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.chieu1);
+            this.panel1.Controls.Add(this.chieu2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(763, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(545, 221);
+            this.panel1.TabIndex = 6;
+            // 
             // Fight
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Snow;
             this.BackgroundImage = global::PokemonProject.Properties.Resources.Fight;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.CancelButton = this.chieu1;
-            this.ClientSize = new System.Drawing.Size(984, 611);
+            this.ClientSize = new System.Drawing.Size(1312, 752);
             this.Controls.Add(this.axWindowsMediaPlayer1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Fight";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Fight";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Fight_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -206,6 +235,7 @@
             this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -224,5 +254,6 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace PokemonProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Sanh());
         }
     }
 }
