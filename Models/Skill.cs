3902b692092d42934887d5e16e6d5d7a24f93a9e
lovell112 <xuansontrannguyen06@gmail.com>
2025-10-22 17:50:24 +0700

    add skill class and systems enum

---
+++ Models/Skill.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+
+namespace PokemonProject.Models
+{
+    public class Skill
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        private float _damage;
+        public float Damage
+        {
+            get => _damage; 
+            set => _damage = value;
+        }
+
+        private PictureBox _image;
+        public PictureBox Image
+        {
+            get => _image;
+            set => _image = value;
+        }
+    }
+}
